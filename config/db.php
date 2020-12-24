a49eaced10341203afa1f20a953d13e9c44e7955
Kevin Gracia <kevin.gracia@pucese.edu.ec>
2020-12-24 17:38:47 -0500

    my first commit

---
+++ config/db.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=db;dbname=yii2basci',
+    'username' => 'root',
+    'password' => 'example',
+    'charset' => 'utf8',
+
+];
+
+?>
